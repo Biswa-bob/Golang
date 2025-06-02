97fad69a49f7c1a3e08f7adc4f304873f983b278
Biswa-bob <biswajeetpanda.bp@gmail.com>
2025-06-02 07:32:59 +0530

    Section-6: Interfaces and Generic Code

---
+++ interfaces/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+	"example.com/note/todo"
+)
+
+type saver interface {
+	Save() error
+}
+
+// type diplayer interface {
+// 	Display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
+func main() {
+	printSomething("Hello")
+	printSomething(1)
+	printSomething(1.2)
+
+	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	outputData(todo)
+
+	outputData(userNote)
+
+}
+
+func printSomething(value interface{}) {
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("Integer: ", intVal)
+		return
+	}
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer: ", value)
+	// case float64:
+	// 	fmt.Println("Float: ", value)
+	// case string:
+	// 	fmt.Println("string: ", value)
+	// }
+}
+
+func outputData(data outputtable) {
+	data.Display()
+	saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return err
+	}
+
+	fmt.Println("Saving the note succeeded!")
+	return nil
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
