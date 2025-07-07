e224d7bb4951ed7529683a0721151ba1445f8d13
Biswa-bob <biswajeetpanda.bp@gmail.com>
2025-07-07 08:54:34 +0530

    Added version 1.0

---
+++ frontend-masters/fem_project/internal/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/Biswa-bob/femProject/internal/app"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
+	return r
+}
